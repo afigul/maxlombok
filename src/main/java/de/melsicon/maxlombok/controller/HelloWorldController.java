19f3ba59fa88e2ae506a4c20d15db4d0459b9d0c
Alexander Figul <alexander.figul@melsicon.de>
2018-10-12 21:04:00 +0200

    add a landing page

--- src/main/java/de/melsicon/maxlombok/controller/HelloWorldController.java
+++ src/main/java/de/melsicon/maxlombok/controller/HelloWorldController.java
@@ -8,7 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @RequestMapping("/")
-    public HelloWorld greeting() {
+    public String greeting() {
+        String text = "Welcome to this little Lombok Example. " +
+                "</br> Urls that work: </br>" +
+                "<a href='http://localhost:8080/showchild'>localhost:8080/showchild</a>" +
+                "<a href='http://localhost:8080/uppercase/{text}'>/uppercase/{text}</a>" +
+                "<a href='http://localhost:8080/json/'>/json</a>";
+        return text;
+    }
+
+    @RequestMapping("/json")
+    public HelloWorld hello() {
         return HelloWorld.builder().build();
     }
 
